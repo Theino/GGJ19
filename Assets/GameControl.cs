b30311d7d829d2dd8a87089dd5e52b1e34f47791
Theino <theino8@yahoo.com>
2019-01-26 00:28:14 -0500

    movement and falling body parts

--- Assets/GameControl.cs
+++ Assets/GameControl.cs
@@ -5,24 +5,45 @@ using UnityEngine;
 public class GameControl : MonoBehaviour {
 
     public GameObject Player;
+    public GameObject BodyPartPool;
+    public GameObject BodyPartPoolActive;
 
-    public float horizontalSpeed = 1;
+    public float horizontalSpeed = 0.05f;
+    public float rotationalSpeed = 1f;
+    public float timeBetweenFalls = 3;
+
+    private float lastFallTime = 0;
+
 
 	// Use this for initialization
 	void Start () {
-		
+        lastFallTime = Time.time;
+        
 	}
 
     // Update is called once per frame
     void Update() {
+        if (Time.time > lastFallTime + timeBetweenFalls)
+        {
+            if (BodyPartPool.transform.childCount > 0)
+            {
+                Transform nextBodyPart = BodyPartPool.transform.GetChild(0);
+                Vector3 bodyPartPosition = new Vector3(Random.Range(-5,5), 5);
+                nextBodyPart.position = bodyPartPosition;
+                nextBodyPart.GetComponent<Rigidbody2D>().simulated = true;
+                nextBodyPart.parent = BodyPartPoolActive.transform;
+                lastFallTime = Time.time;
+            }
+        }
 	}
 
     void FixedUpdate()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
         Vector3 playerPosition = Player.transform.position;
-        playerPosition.x += horizontalSpeed * horizontalInput;
+        playerPosition.x += horizontalSpeed * Input.GetAxis("Horizontal");
         Player.transform.position = playerPosition;
-        Debug.Log(horizontalInput);
+
+        Player.transform.Rotate(0, 0, rotationalSpeed * Input.GetAxis("Rotate"));
+
     }
 }
