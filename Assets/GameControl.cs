aef8917a19fa2b077059561553cc8b66094c9d86
Theino <theino8@yahoo.com>
2019-01-25 23:33:05 -0500

    player moves horizontally

--- Assets/GameControl.cs
+++ Assets/GameControl.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class GameControl : MonoBehaviour {
 
+    public GameObject Player;
+
+    public float horizontalSpeed = 1;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+    // Update is called once per frame
+    void Update() {
 	}
+
+    void FixedUpdate()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 playerPosition = Player.transform.position;
+        playerPosition.x += horizontalSpeed * horizontalInput;
+        Player.transform.position = playerPosition;
+        Debug.Log(horizontalInput);
+    }
 }
