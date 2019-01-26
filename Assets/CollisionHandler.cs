b6277a3f6f2e2117b51344e5adaad3355eb8a100
Theino <theino8@yahoo.com>
2019-01-26 16:03:00 -0500

    stitching animation

---
+++ Assets/CollisionHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionHandler : MonoBehaviour
+{
+
+    public Collision2D collision; [HideInInspector]
+    public bool colliding = false; [HideInInspector]
+    // Use this for initialization
+    void OnCollisionStay2D(Collision2D collisionInfo)
+    {
+        collision = collisionInfo;
+
+    }
+    void OnCollisionEnter2D(Collision2D collisionInfo)
+    {
+        collision = collisionInfo;
+        colliding = true;
+    }
+    void OnCollisionExit2D(Collision2D collisionInfo)
+    {
+        colliding = false;
+    }
+}
--- Assets/GameControl.cs
+++ Assets/GameControl.cs
@@ -7,6 +7,7 @@ public class GameControl : MonoBehaviour {
     public GameObject Player;
     public GameObject BodyPartPool;
     public GameObject BodyPartPoolActive;
+    public GameObject StitchAnimation;
 
     public float horizontalSpeed = 0.05f;
     public float rotationalSpeed = 1f;
@@ -14,7 +15,7 @@ public class GameControl : MonoBehaviour {
 
     private float lastFallTime = 0;
 
-
+    
 	// Use this for initialization
 	void Start () {
         lastFallTime = Time.time;
@@ -22,19 +23,37 @@ public class GameControl : MonoBehaviour {
 	}
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         if (Time.time > lastFallTime + timeBetweenFalls)
         {
             if (BodyPartPool.transform.childCount > 0)
             {
                 Transform nextBodyPart = BodyPartPool.transform.GetChild(0);
-                Vector3 bodyPartPosition = new Vector3(Random.Range(-5,5), 5);
+                Vector3 bodyPartPosition = new Vector3(Random.Range(-5, 5), 5);
                 nextBodyPart.position = bodyPartPosition;
                 nextBodyPart.GetComponent<Rigidbody2D>().simulated = true;
                 nextBodyPart.parent = BodyPartPoolActive.transform;
                 lastFallTime = Time.time;
             }
         }
+
+        if (Input.GetAxis("Attach") > 0)
+        {
+            foreach (Transform activeBodyPart in BodyPartPoolActive.transform)
+            {
+                CollisionHandler collisionHandler = activeBodyPart.GetComponent<CollisionHandler>();
+                if (collisionHandler.colliding)
+                {
+                    activeBodyPart.parent = Player.transform;
+                    Vector2 contactPoint = collisionHandler.collision.contacts[0].point;
+
+                    Instantiate(StitchAnimation, new Vector3(contactPoint.x, contactPoint.y), Quaternion.identity, Player.transform);
+                    Destroy(activeBodyPart.GetComponent<Rigidbody2D>());
+                }
+            }
+        }
+            
 	}
 
     void FixedUpdate()
